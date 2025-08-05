77f3a0ce9d10f56e3ba255e3596110e794537921
HninLaeKhaing <169078968+HninLaeKhaing@users.noreply.github.com>
2025-08-05 13:08:14 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,20 +1,14 @@
-import json
 import random
-import numpy as np
+import json
 import pickle
-from nltk.stem import WordNetLemmatizer
-from keras.models import load_model
+import numpy as np
 import nltk
-import os
-
-# Set up NLTK download directory for Streamlit Cloud compatibility
-NLTK_DATA_PATH = os.path.join(os.path.dirname(__file__), "nltk_data")
-os.makedirs(NLTK_DATA_PATH, exist_ok=True)
-nltk.download('punkt', download_dir=NLTK_DATA_PATH)
-nltk.download('wordnet', download_dir=NLTK_DATA_PATH)
-nltk.data.path.append(NLTK_DATA_PATH)
+from keras.models import load_model
+from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
+
+# Load required files
 model = load_model('model.h5')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('texts.pkl', 'rb'))
@@ -25,27 +19,30 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
+# Return bag of words array
 def bow(sentence, words):
     sentence_words = clean_up_sentence(sentence)
-    bag = [0] * len(words)
+    bag = [0]*len(words)
     for s in sentence_words:
-        for i, w in enumerate(words):
+        for i,w in enumerate(words):
             if w == s:
                 bag[i] = 1
     return np.array(bag)
 
+# Predict the intent
 def predict_class(sentence):
     p = bow(sentence, words)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
-    return_list = [{"intent": classes[r[0]], "probability": str(r[1])} for r in results]
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+# Get response
 def get_response(ints, intents_json):
-    if not ints:
-        return "I'm here to listen. Tell me more."
     tag = ints[0]['intent']
     for i in intents_json['intents']:
         if i['tag'] == tag:
@@ -53,4 +50,15 @@ def get_response(ints, intents_json):
 
 def chatbot_response(msg):
     ints = predict_class(msg)
-    return get_response(ints, intents)
+    if ints:
+        res = get_response(ints, intents)
+        return res
+    else:
+        return "I'm not sure how to respond to that. Could you please rephrase?"
+
+# Test it
+while True:
+    message = input("You: ")
+    if message.lower() == "quit":
+        break
+    print("Bot:", chatbot_response(message))
